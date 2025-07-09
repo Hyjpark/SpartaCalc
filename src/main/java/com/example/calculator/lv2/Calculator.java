0ab3cca419d491ecb3844430490309a6e6764601
Hyjpark <hyej042@gmail.com>
2025-07-09 17:57:23 +0900

    feat: Calculator 클래스 생성
    - 사칙연산 수행 후, 결과값 반환하는 메서드 구현
    - 연산 결과를 저장하는 컬렉션 타입 필드 추가

---
+++ src/main/java/com/example/calculator/lv2/Calculator.java
@@ -0,0 +1,39 @@
+package com.example.calculator.lv2;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class Calculator {
+    // 연산 결과를 저장하는 컬렉션 타입 필드
+    List<Integer> calcResult = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char operator) {
+        int result = 0;
+
+        try {
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        } catch (ArithmeticException e) {
+            System.out.println("나눗셈 연산에서 분모(두번재 정수)에 0이 입력될 수 없습니다.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("잘못된 연산자입니다.");
+        }
+
+        return result;
+    }
+}
