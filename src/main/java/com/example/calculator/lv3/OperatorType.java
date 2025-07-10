e4be774aa15d81d5975805099a8518eaa8eef1ad
Hyjpark <hyej042@gmail.com>
2025-07-10 14:46:38 +0900

    feat: 람다 OperatorType 클래스 람다식 적용

--- src/main/java/com/example/calculator/lv3/OperatorType.java
+++ src/main/java/com/example/calculator/lv3/OperatorType.java
@@ -1,29 +1,29 @@
 package com.example.calculator.lv3;
 
+// 람다식 구현을 위한 함수형 인터페이스 활용
+import java.util.function.BiFunction;
+
 // Enum 타입을 활용하여 연산자 타입에 대한 정보를 관리
 public enum OperatorType {
-    PLUS("+") {
-        public int calc(int num1, int num2) { return num1 + num2; }
-    },
-    MINUS("-") {
-        public int calc(int num1, int num2) { return num1 - num2; }
-    },
-    MULTIPLY("*") {
-        public int calc(int num1, int num2) { return num1 * num2; }
-    },
-    DIVIDE("/") {
-        public int calc(int num1, int num2) { return num1 / num2; }
-    };
+    PLUS("+", (num1, num2) -> num1 + num2),
+    MINUS("-",  (num1, num2) -> num1 - num2),
+    MULTIPLY("*",  (num1, num2) -> num1 * num2),
+    DIVIDE("/",  (num1, num2) -> num1 / num2),;
 
     // 매핑을 위한 필드 값 추가
     private final String operator;
+    private final BiFunction<Double, Double, Double> operation;
 
     // 생성자
-    OperatorType(String operator) {
+    OperatorType(String operator, BiFunction<Double, Double, Double> operation) {
         this.operator = operator;
+        this.operation = operation;
     }
 
-    public abstract int calc(int num1, int num2);
+    // 매개변수로 받은 타입 상관 없이 더블로 반환
+    public Double calc(Number num1, Number num2) {
+        return operation.apply(num1.doubleValue(), num2.doubleValue());
+    };
 
     @Override
     public String toString() {
